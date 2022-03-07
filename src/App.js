a2624718a94f0084331b919e1f11a29c28872ba2
Thomas Delargy <tdelar@uw.edu>
2022-03-06 20:49:33 -0800

    with map

--- src/App.js
+++ src/App.js
@@ -1,10 +1,23 @@
 import logo from "./logo.svg";
 import "./App.css";
+import * as React from "react";
+import Map from "react-map-gl";
 
 function App() {
   return (
     <div className="App">
-      <p>Hello</p>
+      <p>new</p>
+      <Map
+        initialViewState={{
+          longitude: -100,
+          latitude: 40,
+          zoom: 3.5,
+        }}
+        style={{ width: "100vw", height: "100vh" }}
+        mapStyle="mapbox://styles/mapbox/streets-v9"
+        mapboxAccessToken="pk.eyJ1IjoidG9tbXlkZWwiLCJhIjoiY2t4NW4xbG9zMmR2aTJ1cDJnM2xuemd6diJ9.9YwDOFlOAN7Yn6rXYTSt-w"
+      />
+      ;
     </div>
   );
 }
